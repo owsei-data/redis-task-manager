62db1ff9fc30c2d3e87df6b0b322d910f88aaa48
Pedro Jorge <pedrojorgedesouzacolombrino@gmail.com>
2025-07-14 18:00:59 -0300

    rota flask main

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-from flask import flask
+from flask import Flask
+from flask import render_template as rt #renderizar o html
+
+app = Flask(__name__)
+
+@app.route("/") #rota raiz flask
+def home():
+    return rt("main.html") #return do html main
+
