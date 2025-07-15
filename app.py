3bd75f8a47f1b8468073fad24a9b71b63032aca8
Pedro Jorge <pedrojorgedesouzacolombrino@gmail.com>
2025-07-14 22:59:37 -0300

    add porta railway app.py

--- app.py
+++ app.py
@@ -7,3 +7,6 @@ app = Flask(__name__)
 def home():
     return rt("main.html", tarefas=tarefas) #return do html main
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
