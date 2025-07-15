a7531387e1c2cfdec2af25671410815ba95a6344
Pedro Jorge <pedrojorgedesouzacolombrino@gmail.com>
2025-07-14 22:52:39 -0300

    removendo redis

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask import render_template as rt #renderizar o html
-import redis_client
 
 app = Flask(__name__)
 
 @app.route("/") #rota raiz flask
 def home():
-    tarefas = redis_client.mostrar_tarefas()
     return rt("main.html", tarefas=tarefas) #return do html main
 
