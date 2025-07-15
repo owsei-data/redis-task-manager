3dad6714cdaabae4e60ae1affbc02174a2e49df5
Pedro Jorge <pedrojorgedesouzacolombrino@gmail.com>
2025-07-15 18:29:06 -0300

    auth.py, finalizacao do crud_task

--- app/auth.py
+++ app/auth.py
@@ -1,8 +1,39 @@
 from pymongo import MongoClient
 import os
+import bcrypt
+import datetime
 
 mongo_uri = os.getenv("MONGO_URI") #variavel local railway
 
 #settings
-db = client('task-manager') #set db
-colecao = db['user']
+client = MongoClient(mongo_uri)
+db = client['task-manager'] #set db
+colecao_user = db['user']
+
+#registro
+def registrar_user(login, senha):
+    data_registro = datetime.datetime.now()
+    try:
+
+        senha_bytes = senha.encode('utf-8') #encodando pra bytes
+        hash_senha = bcrypt.hashpw(senha_bytes, bcrypt.gensalt()) #encriptando a senha
+        
+        str_hash = hash_senha.decode('utf-8') #decodando
+
+        r = colecao_user.insert_one({'login':login,'senha':str_hash}, data_criacao = data_registro) #setando no banco com senha criptada e decodada
+        return True #retorno console
+    except Exception as e:
+        return False #retorno console
+
+
+def verificar_senha(senha_fornecida, hash_armazenado):
+    senha_bytes = senha_fornecida.encode('utf-8')
+    hash_bytes = hash_armazenado.encode('utf-8')
+    return bcrypt.checkpw(senha_bytes, hash_bytes) #check do bcrypt - retorna true se igual
+
+'''
+r = colecao_user.find_one({'login': 'edimilson'})
+print(r)
+
+r2 = verificar_senha('123batata','$2b$12$r1AwkZsp6bZKi.5vD2sjk.lyAU2iSoopq.ylZaFA7Rc17vms/jKYq')
+print(r2)'''
--- app/crud_tasks.py
+++ app/crud_tasks.py
@@ -1,19 +1,20 @@
 from pymongo import MongoClient
 import os
-import datetime as dt #pegar o horario de criacao das tasks
+import datetime #pegar o horario de criacao das tasks
 from bson.objectid import ObjectId #para convert do objectid dos registros task
+from bson.errors import InvalidId
 
 mongo_uri = os.getenv('MONGO_URI')#env variable do railway
 client = MongoClient(mongo_uri)
 
-db = client('task-manager') #db atlas
+db = client['task-manager'] #db atlas
 colecao_tarefa = db['tarefas'] #tabela
 
 #func crud para tarefas
 
 #C - create
 def criar_tarefa(titulo,descricao,deadline,status = 'Pendente'): #criar futuramente a conexao entre usuario e task
-    data_criacao = dt.datetime.now().strftime('%d-%m-%y')
+    data_criacao = datetime.datetime.now() #datetime puro pro mongo
     tarefa ={ #dicionario
         'titulo' : titulo,
         'descricao' : descricao,
@@ -21,17 +22,69 @@ def criar_tarefa(titulo,descricao,deadline,status = 'Pendente'): #criar futurame
         'deadline' : deadline,
         'status' : status
     }
-    registrar = colecao_tarefa.insert_one(tarefa) #registrando tarefa
-    return str(registrar.inserted_id) #retornando id
+    r = colecao_tarefa.insert_one(tarefa) #registrando tarefa
+    return str(r.inserted_id) #retornando id
 
 #R - read
 def ler_tarefa(id_tarefa):
-    tarefa = colecao_tarefa.find_one({'_id': ObjectId(id_tarefa)}) #conversao
-    if tarefa:
-        return tarefa #retorna registro tarefa
-    else:
-        return None #none se n encontrado
+    try:
+        tarefa = colecao_tarefa.find_one({'_id': ObjectId(id_tarefa)}) #conversao
+        if tarefa:
+            return tarefa #retorna registro tarefa
+        else:
+            return None #none se n encontrado
+    except InvalidId:
+        print('id invalido')
+        return None
+
 #U - update
+def atualizar_tarefa(id_tarefa, campo, novo_valor):
+    try:
+        filtro = {'_id': ObjectId(id_tarefa)} #filtro por id
+        update = {'$set': {campo: novo_valor}} #update do valor da tarefa
 
+        r = colecao_tarefa.update_one(filtro,update)
+        if r.matched_count > 0: #verificando quantos docs foram modificados
+            print(f"atualizada: {r.matched_count}")
+            return True
+        else:
+            print("nao encontrada")
+            return False
+    except InvalidId:
+        print('id invalido')
+        return False
 
+#D - delete
+def deletar_tarefa(id_tarefa):
 
+    try:
+        filtro = {'_id': ObjectId(id_tarefa)} #filtro id
+        
+        r = colecao_tarefa.delete_one(filtro) #result
+
+        if r.deleted_count > 0: #verificando quantos docs foram modificados
+            print(f"deletada: {r.deleted_count}")
+            return True
+        else:
+            print("nao encontrada")
+            return False
+    except InvalidId:
+        print('id invalido')
+        return None
+'''
+tarefa100 = criar_tarefa("aprender python", "quero aprender python", "10/12/2025")
+r1 = colecao_tarefa.find_one({'titulo': 'aprender python'})
+print(r1)
+
+r2 = ler_tarefa("6876c0401a80747da3886dc9")
+
+r2 = ler_tarefa("6876c0401a80747da3886dc9")
+print(r2)
+
+r3 = atualizar_tarefa("6876c0401a80747da3886dc9", "titulo", "aprender python 2x mais")
+print(r3)
+tarefinha = colecao_tarefa.find_one({'_id': ObjectId('6876c0401a80747da3886dc9')})
+print(tarefinha)
+
+r4 = deletar_tarefa("6876c0401a80747da3886dc9")
+print(f'deletado: {r4}')'''
